60b85a0cc6033e610f284f2eb4e7250730bf19b1
Vincent Sandt <vincent@tetraed.re>
2020-03-25 03:44:21 +0100

    feat: parse markdown

--- src/code.ts
+++ src/code.ts
@@ -1,28 +1,98 @@
-import { defaultBlockParse as mdParse } from 'simple-markdown';
+import unified from 'unified';
+import markdown from 'remark-parse';
 figma.showUI(__html__);
 
-function nodeReducer(accumulator, currentNode, currentIndex) {
-  switch (currentNode.type) {
-    case 'text':
-      const text = figma.createText();
-      text.characters = currentNode.content;
-      text.y = currentIndex * 20;
-      break;
-    default:
-      if (currentNode.content) {
-        return currentNode.content.reduce(nodeReducer);
-      }
-      break;
+function mdParse(content): any {
+  return unified()
+    .use(markdown, { commonmark: true })
+    .parse(content);
+}
+
+//  .use(remark2rehype)
+//  .use(html)
+
+function createFrame(items, itemSpacing = 10) {
+  const frame = figma.createFrame();
+  items.forEach(item => frame.appendChild(item));
+  frame.resize(400, 1);
+  frame.layoutMode = 'VERTICAL';
+  frame.itemSpacing = itemSpacing;
+  frame.fills = [{ ...frame.fills[0], visible: false }];
+  return frame;
+}
+
+function createTextNode(characters) {
+  const text = figma.createText();
+  text.resize(400, 1);
+  text.textAutoResize = 'HEIGHT';
+  text.characters = characters;
+  return text;
+}
+
+function nodeReducer(
+  params: { textWidth: number; yValue?: number } = {
+    textWidth: 200,
   }
-  return accumulator;
+) {
+  return (accumulator, currentNode, currentIndex) => {
+    let text: TextNode;
+    const { type } = currentNode;
+    try {
+      switch (type) {
+        case 'text':
+          return [...accumulator, currentNode.value];
+
+        case 'emphasis':
+        case 'strong':
+          const textContent = currentNode.value
+            ? currentNode.value
+            : currentNode.children.reduce(nodeReducer({ ...params }), []).join('');
+          return [...accumulator, `[${type}]${textContent}[/${type}]`];
+
+        case 'break':
+          return [...accumulator, '\n'];
+
+        case 'heading':
+        case 'paragraph':
+          const content = currentNode.children
+            .reduce(nodeReducer({ ...params }), [])
+            .join('');
+          const pattern = /(\[(.*)\].*\[\/(.*)\])/gi;
+          const matches = content.match(pattern);
+          text = createTextNode(content);
+          text.textStyleId = type + '-' + currentNode.depth;
+          return [...accumulator, text];
+
+        case 'list':
+        case 'blockquote':
+        case 'listItem':
+          const items = currentNode.children.reduce(nodeReducer(params), []);
+
+          const frame = createFrame(items);
+          return [...accumulator, frame];
+
+        default:
+          throw new Error(`missing type: ${type}`);
+      }
+    } catch (e) {
+      throw Object.assign(e, { currentNode });
+    }
+  };
 }
 
 figma.ui.onmessage = msg => {
   if (msg.type === 'mi-parse-file') {
     (async () => {
-      const content = mdParse(msg.content);
       await figma.loadFontAsync({ family: 'Roboto', style: 'Regular' });
-      console.log(content.reduce(nodeReducer));
+      try {
+        const content = mdParse(msg.content).children;
+        const items = content.reduce(nodeReducer({ textWidth: 300 }), []);
+        const frame = createFrame(items);
+        frame.itemSpacing = 10;
+        figma.currentPage.selection.slice().push(frame);
+      } catch (e) {
+        console.error(e);
+      }
     })();
   }
 
@@ -31,7 +101,7 @@ figma.ui.onmessage = msg => {
   }
 
   if (msg.type === 'mi-error') {
-    console.log(msg);
+    console.error(msg);
     figma.closePlugin();
   }
 };
--- src/lib/markdownFileUpload.ts
+++ src/lib/markdownFileUpload.ts
@@ -1,5 +1,4 @@
 export default async function markdownFileUpload(file) {
-  console.log('markdownFileUpload');
   return new Promise<string>(resolve => {
     const reader = new FileReader();
     reader.onload = ({ target }: any) => resolve(target.result);
